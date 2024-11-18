723ec5bfaf4c7090a5155cbb3257077003268993
ngntu10 <phamnguyentu04@gmail.com>
2024-11-18 18:24:44 +0700

    Add JHipster-generated backend structure

---
+++ src/main/java/com/springapp/repository/rowmapper/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Webflux database column mapper.
+ */
+package com.springapp.repository.rowmapper;
---
+++ src/main/java/com/springapp/service/mapper/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data transfer objects mappers.
+ */
+package com.springapp.service.mapper;
